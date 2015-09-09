9b9561846a19d91afc2b6ea691019c8aa15e294c
WillDiggle <willdiggle.phd@gmail.com>
2015-09-08 22:18:50 -0500

    big
    BIG

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-document.write("rooster dick lol");
+var nips = function();
+{
+  var name = prompt("what name faggot");
+  if (name.length<=5)
+  {
+    document.write("nice and short, just how I like it");
+  }
+  else
+  {
+    document.write("nah, too long. i'll call you fig.");
+    name = "fig";
+  }
+}
