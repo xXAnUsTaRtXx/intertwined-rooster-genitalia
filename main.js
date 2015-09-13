cc03cf321d6b99a4fab46bd2bcadb248083b7cd6
WillDiggle <willdiggle.phd@gmail.com>
2015-09-12 20:01:31 -0500

    nips

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function n() {document.write("<br>");} //makes a new line
 function start()
 {
   var name = prompt("What's your name?");
+  document.write("<div class=\"start\">");
   if (name.length<=3)
   {
     document.write("<p>"+name+", huh? Nice and short, just how I like it.</p>");
@@ -16,4 +17,5 @@ function start()
   document.write("<p>So, "+name+", what do you remember about yourself?</p>")
   document.getElementsByTagName("head")[0].innerHTML = "<title>Intertwined Rooster Genitalia</title>\
   <link rel=\"stylesheet\" type=\"text/css\" href=\"main.css\">";
+  document.write("</div>");
 }
